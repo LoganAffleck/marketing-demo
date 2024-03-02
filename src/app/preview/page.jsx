6e707ea60848de277c0a66b452f1353bd80c82eb
J. Logan Affleck <affleck.logan@gmail.com>
2024-03-02 09:56:41 -0500

    Add preview

---
+++ src/app/preview/page.jsx
@@ -0,0 +1,21 @@
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "../../components/builder";
+
+// Builder Public API Key set in .env file
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
+
+export default async function Page(props) {
+  const builderModelName = "figma-imports";
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get(builderModelName)
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} model={builderModelName} />
+    </>
+  );
+}
